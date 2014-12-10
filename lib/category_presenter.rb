ecc68697ff10a0ba79e04a77d30abf058224ceba
Volkan Unsal <spocksplanet@gmail.com>
2014-12-10 16:10:25 -0500

    Add categories

--- lib/category_presenter.rb
+++ lib/category_presenter.rb
@@ -1,11 +1,15 @@
 class CategoryPresenter < Struct.new(:row)
   def to_category
-    return parent_category(row).children.create(row) if row[:parent_id].present?
+    begin
+      return parent_category(row).children.create(row) if row[:parent_id].present?
+    rescue Exception => e
+      binding.pry
+    end
+
     Category.create(row)
   end
 
   def parent_category(row)
-    Category.find_or_create_by(name: row[:parent_name],
-                               taxonomy_id: row[:parent_id])
+    Category.find_or_create_by!(name: row[:parent_name], taxonomy_id: row[:parent_id])
   end
 end
